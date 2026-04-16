706db6e35d70a14110a1dad45f883bdbe9e6727d
SumayaM <epicgamer50012@gmail.com>
2026-04-17 00:08:25 +0100

    created line chart which visualises pink morsel sale data - shown on dash app

--- app.py
+++ app.py
@@ -5,18 +5,65 @@
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import csv
+import os
+
+saleslist = []
+datelist = []
+regionlist = []
+
+def readfile (csvreader):
+
+    for row in csvreader:
+
+        if row[0] == "product":
+            continue
+
+        product = row[0]
+        price = row[1]
+        quantity = row[2]
+        date = row[3]
+        region = row[4]
+
+        actualprice = price[1:]
+        sales = int(quantity) * float(actualprice)
+        if product == "pink morsel":
+            saleslist.append(sales)
+            datelist.append(date)
+            regionlist.append(region)
+
+
+
+
+
+
+
+dir = os.path.dirname(__file__)
+
+with open(os.path.join(dir, 'data' ,'daily_sales_data_0.csv')) as csv_file1, open(os.path.join(dir, 'data' ,'daily_sales_data_1.csv')) as csv_file2, open(os.path.join(dir, 'data' ,'daily_sales_data_2.csv')) as csv_file3:
+    csv_reader1 = csv.reader(csv_file1, delimiter=',')
+    csv_reader2 = csv.reader(csv_file2, delimiter=',')
+    csv_reader3 = csv.reader(csv_file3, delimiter=',')
+
+
+    print("Product: Pink Morsels")
+    print("Sales       date       region")
+    readfile(csv_reader1)
+    readfile(csv_reader2)
+    readfile(csv_reader3)
+
 
 app = Dash()
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
-    "Sales": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Date": [4, 1, 2, 2, 4, 5],
-    "Region": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "Date": datelist,
+    "Sales": saleslist,
+    "Region": regionlist
 })
 
-fig = px.line(df, x="Sales", y="Date", color="Region")
+fig = px.line(df, x="Date", y="Sales", color="Region")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
